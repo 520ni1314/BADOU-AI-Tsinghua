43720788df93a25005e8932ad62f480fc7d69a79
Dan Mo <danmo2060@gmail.com>
2021-11-24 18:14:10 +1100

    lenna gray

---
+++ 7-莫丹-澳洲/第一周/2-nearest-interpolation.py
@@ -0,0 +1,16 @@
+# import color convert tool (scikit image)
+from skimage.color import rgb2gray
+# import number crunching tool
+import numpy as np
+# import plotting tool
+import matplotlib.pyplot as plt
+# import image import tool (pillow and opencv)
+from PIL import Image
+
+### RGB to Gray method 1, manual calculation ###
+
+
+### RGB to Gray method 2, use module ###
+
+
+### Binarization ###
