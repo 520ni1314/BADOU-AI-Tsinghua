ded2bc397bb565dba58e46f6fc1eb853520ca3ca
Chenyyvista <928932170@qq.com>
2021-11-25 21:23:40 +0800

    第一次作业-最近邻插值

--- 151-陈扬洋-上海/第一周/hw02-nearest_interpolation.py
+++ 151-陈扬洋-上海/第一周/hw02-nearest_interpolation.py
@@ -3,19 +3,39 @@ import numpy as np
 
 
 def nearest_interpolation(img, target_size: tuple):
-    height, width, channel = img.shape
-    target_size = (target_size[0], target_size[1], channel)
-    img_new = np.zeros(target_size, dtype=img.dtype)
+    # 目标尺寸
+    h, w = target_size
 
-    ratio_h = target_size[0] / height
-    ratio_w = target_size[1] / width
+    # 原始尺寸
+    if len(img.shape) > 2:
+        height, width, channel = img.shape
+    else:
+        height, width = img.shape
+        channel = 1
 
-    for i in range(0, target_size[0]):
-        for j in range(0, target_size[1]):
-            print()
+    # 缩放比率
+    ratio_h = h / height
+    ratio_w = w / width
+
+    # 生成坐标矩阵
+    y_index = np.arange(h, dtype=np.int32)
+    x_index = np.arange(w, dtype=np.int32)
+    y_target_index = np.int32(y_index / np.array(ratio_h))
+    x_target_index = np.int32(x_index / np.array(ratio_w))
+
+    # 根据像素个数指定原图坐标
+    x_target_index = np.tile(x_target_index, h)
+    y_target_index = y_target_index.repeat(w)
+
+    img_new = img[y_target_index, x_target_index]
+    img_new = img_new.reshape(h, w, channel)
+
+    cv2.imshow("resized", img_new)
+    cv2.waitKey(0)
 
 
 if __name__ == '__main__':
     img_path = "./pics/lenna.png"
     img_bgr = cv2.imread(img_path)
-    nearest_interpolation(img_bgr, [128, 128])
+    img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
+    nearest_interpolation(img_gray, (680, 400))
