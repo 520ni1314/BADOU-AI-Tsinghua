502570394fabb645b3ef1c9b6bc77630dde59404
linmb <linmb@akulaku.com>
2021-12-24 10:08:09 +0800

    第四周作业 canny  透视变换 实现

--- 43-林茂彬-深圳/test.py
+++ 43-林茂彬-深圳/test.py
@@ -1,15 +1,8 @@
 import cv2
 import numpy as np
+import random
 
 
-img = cv2.imread('lenna.png',0)
-
-
-
-#获取图像高度、宽度
-rows, cols = img.shape[:]
-data = img.reshape((rows * cols, 1))
-data = np.float32(data)
-print(data)
+print(random.gauss(2,3))
 
 
