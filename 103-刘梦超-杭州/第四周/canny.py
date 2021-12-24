3dcd08757deadbe6e90e32bf53c56b0ce3e4363e
lmcjrrg <95133674+lmcjrrg@users.noreply.github.com>
2021-12-24 17:51:49 +0800

    canny需要灰度图

--- 103-刘梦超-杭州/第四周/canny.py
+++ 103-刘梦超-杭州/第四周/canny.py
@@ -4,8 +4,8 @@ import cv2
 
 # 读入原图
 img = cv2.imread("lenna.png")
-# opencv读进来的图片通道排列是BGR,需要转为RGB
-trans_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# 需要灰度图
+trans_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # 调用canny接口
 edge_img = cv2.Canny(trans_rgb, 0, 300)
 cv2.imshow("edge_img", edge_img)
